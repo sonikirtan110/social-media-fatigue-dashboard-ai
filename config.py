c0dce658caa685e7c4da881f00c5afb815dbb292
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-02-13 20:11:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,7 @@
-import mysql.connector
-from mysql.connector import Error
-
-def create_connection():
-    try:
-        conn = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="1978",
-            database="social",
-            port=3310  # Default MySQL port (adjust if needed)
-        )
-        if conn.is_connected():
-            print("Connected to MySQL database")
-            return conn
-    except Error as e:
-        print(f"Error connecting to MySQL: {e}")
-        return None
+DB_CONFIG = {
+    "host": "localhost", 
+    "port": 3310,  
+    "user": "root",
+    "password": "1978",
+    "database": "social"
+}
