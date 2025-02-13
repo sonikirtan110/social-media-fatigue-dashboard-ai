c39e77e302b7cf02ca56aa67160ed27813f63b7f
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-02-13 19:14:36 +0530

    Delete SAP Project directory

--- SAP Project/config.py
+++
@@ -1,8 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': '1978',
-    'database': 'social',
-    'port': 3310,  # Updated port
-    'auth_plugin': 'mysql_native_password'
-}
