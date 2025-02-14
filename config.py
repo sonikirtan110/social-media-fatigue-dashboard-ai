be31f816219d8d99d9d2f80139b7e5d5a1982825
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-02-14 19:23:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ DB_CONFIG = {
     'host': os.environ.get('DB_HOST', 'dpg-cunkaeggph6c73eujvqg-a'),
     'port': int(os.environ.get('DB_PORT', 5432)),
     'user': os.environ.get('DB_USER', 'root'),
-    'password': os.environ.get('DB_PASSWORD', 'your_password_here'),
+    'password': os.environ.get('DB_PASSWORD', 'dmTVQlqg7dgX7HvvIFGPA3tkoClUv1ZW'),
     'database': os.environ.get('DB_NAME', 'social_4stp')
 }
