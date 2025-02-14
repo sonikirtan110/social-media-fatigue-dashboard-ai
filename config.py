182561490542d731b4f59de8f940b3700c4ef917
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-02-14 19:14:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+# config.py
 DB_CONFIG = {
-    'host': 'dpg-cunkaeggph6c73eujvqg-a',
-    'database': 'social_4stp',
+    'host': 'dpg-cunkaeggph6c73eujvqg-a',  # Render internal host
+    'port': 5432,
     'user': 'root',
-    'password': 'dmTVQlqg7dgX7HvvIFGPA3tkoClUv1ZW',
-    'port': 5432
+    'password': 'dmTVQlqg7dgX7HvvIFGPA3tkoClUv1ZW',  # Your Render password
+    'database': 'social_4stp'
 }
