93b33ab2bfe39a467883cfa98749dc7b226f86fa
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-02-14 07:45:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 DB_CONFIG = {
-    'host': os.environ.get('DB_HOST', 'localhost'),
+    'host': os.environ.get('DB_HOST', '127.0.0.1'),
     'user': os.environ.get('DB_USER', 'root'),
     'password': os.environ.get('DB_PASSWORD', '1978'),
     'database': os.environ.get('DB_NAME', 'social'),
