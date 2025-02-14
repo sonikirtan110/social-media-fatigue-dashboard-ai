3b02a42e9adab35b67dd1410a8ba0f37930d9dbf
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-02-14 17:10:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 DB_CONFIG = {
-    'host': 'localhost',  # ngrok hostname
-    'user': 'root',
-    'password': '1978',
-    'database': 'social',
-    'port': 3310,  # ngrok forwarded port
+    'host': 'mysql-8cea257-sonikirtan110-eedc.h.aivencloud.com',  # Aiven host
+    'user': 'avnadmin',            # Default admin user
+    'password': 'AVNS_qmLnbGkGDN5w069YjCs',   # Your password
+    'database': 'social',       # Default database name
+    'port': 10102,                 # Aiven's MySQL port
     'auth_plugin': 'mysql_native_password'
 }
