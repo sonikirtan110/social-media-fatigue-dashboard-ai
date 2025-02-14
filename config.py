42cde0ada2245b056c3fae41320653b195373533
Kirtan Soni <118914256+sonikirtan110@users.noreply.github.com>
2025-02-14 09:49:59 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 DB_CONFIG = {
-    'host': '0.tcp.ngrok.io',  # ngrok hostname
+    'host': '103.26.49.80',  # ngrok hostname
     'user': 'root',
     'password': '1978',
     'database': 'social',
-    'port': 12345,  # ngrok forwarded port
+    'port': 3310,  # ngrok forwarded port
     'auth_plugin': 'mysql_native_password'
 }
